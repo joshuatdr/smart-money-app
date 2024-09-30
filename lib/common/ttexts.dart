41191c9ca39b7bab353758c582bb717a796f8549
Abdurahman <abdi_mo4@outlook.com>
2024-09-30 15:01:22 +0100

    created signup page

--- lib/common/ttexts.dart
+++ lib/common/ttexts.dart
@@ -1,13 +1,18 @@
 // This class contains all the text details in String format
 
-class TTexts{
-  // -- Social Logos
+class TTexts {
+  // -- SignInPage
   static const String loginTitle = "Login";
-  static const String loginSubTitle = "Please signin to continue";
+  static const String loginSubTitle = "Please sign in to continue";
   static const String email = "email";
   static const String password = "password";
   static const String rememberMe = "remember me";
   static const String forgotPassword = "forgot password";
-  static const String signin = "signin";
-  static const String createAccount = "create account";
+static const String signIn = "Sign in";
+  // -- SignUpPage
+  static const String signUp = "Sign up";
+  static const String createAccount = "Create account";
+  static const String friendlyName = "Nick name";
+   
+  static const String terms = "Terms and Conditions";
 }
