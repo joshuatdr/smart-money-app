2b2a3d372ee4aa045fe8c47f920c4c8e23f696da
jasonic69 <justyhill3@hotmail.com>
2024-10-06 00:20:50 +0100

    transaction delete functionality added

--- lib/pages/history.dart
+++ lib/pages/history.dart
@@ -1,14 +1,61 @@
-//import 'dart:io';
-
 import 'package:flutter/material.dart';
 import '../model/transactions.dart';
 import '../services/api.dart';
 import 'package:provider/provider.dart';
 import '../providers/user_provider.dart';
-// =======
-// import 'package:smart_money_app/providers/user_provider.dart';
-// import 'package:provider/provider.dart';
-// >>>>>>> main
+import 'package:http/http.dart' as http;
+
+Future<void> deleteTransaction(int transactionId, int userId) async {
+  final response = await http.delete(Uri.parse(
+      "https://smart-money-backend.onrender.com/api/user/$userId/transactions/$transactionId"));
+  if (response.statusCode == 204) {
+    // If the server returns a 204 response, user is successfully deleted
+  }
+
+  print('deleteTransaction');
+}
+
+Future promptTransaction(context, data, userId) async {
+  await showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Center(child: const Text('Delete Transaction')),
+        content: SizedBox(
+           width: 100,
+        height: 200,
+          child: Column(
+            children:  [
+                            Text(data.name),
+                           // Text(data.cost),
+                           // Text(data.created_at),
+                                
+              const Text(
+                  'Do you want to delete this transaction?'),
+            ],
+          ),
+        ),
+            
+        actions: <Widget>[
+          TextButton(
+            child: const Text('Cancel'),
+            onPressed: () {
+              Navigator.of(context).pop(false); // closes prompt
+            },
+          ),
+          TextButton(
+            child: const Text('Delete'),
+            onPressed: () async {
+              await deleteTransaction(data.transactionId, userId).then((value) => {
+                    if (context.mounted) {Navigator.of(context).pop()}
+                  });
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
 
 class HistoryScreen extends StatefulWidget {
   HistoryScreen({super.key});
@@ -18,29 +65,38 @@ class HistoryScreen extends StatefulWidget {
 }
 
 class _HistoryScreenState extends State<HistoryScreen> {
-  List results = [];
-  DataRow _getDataRow(index, data) {
-    return DataRow(
-      cells: <DataCell>[
-        DataCell(Text(data.name)), //add name of your columns here
-        DataCell(Text('£${data.cost}')),
-        DataCell(Text(data.createdAt)),
-        DataCell(Icon(Icons.picture_in_picture, color: Colors.red[500])),
-      ],
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     AsyncSnapshot.waiting();
     var userId = context.watch<UserProvider>().userID;
+
+    DataRow getDataRow(index, data) {
+      return DataRow(
+        cells: <DataCell>[
+          DataCell(Text(data.name)), //add name of your columns here
+          DataCell(Text('£${data.cost}')),
+          DataCell(Text(data.createdAt)),
+          DataCell(
+            Icon(Icons.image_outlined, color: Colors.red[500]),
+          ),
+          DataCell(
+            Icon(Icons.delete_forever, color: Colors.red[500]),
+            onTap: () async {
+              await promptTransaction(context, data, userId)
+                  .then((value) => {setState(() {})});
+            },
+          ),
+        ],
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
         centerTitle: true,
         backgroundColor: Colors.orange,
-        title: Text("Transaction History",
-            style: TextStyle(color: Colors.white)),
+        title:
+            Text("Transaction History", style: TextStyle(color: Colors.white)),
         actions: <Widget>[
           IconButton(
               icon: const Icon(Icons.edit),
@@ -71,15 +127,15 @@ class _HistoryScreenState extends State<HistoryScreen> {
                   ),
                   columnSpacing: 30,
                   columns: [
-                    
                     DataColumn(label: Text('Name')),
                     DataColumn(label: Text('Cost')),
                     DataColumn(label: Text('Date')),
                     DataColumn(label: Text('Image')),
+                    DataColumn(label: Text('Del')),
                   ],
                   rows: List.generate(
                     results.length,
-                    (index) => _getDataRow(
+                    (index) => getDataRow(
                       index,
                       results[index],
                     ),
