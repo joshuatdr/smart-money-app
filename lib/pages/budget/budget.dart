3bdeb21d377acb22af63d03e04a9ee1de433a4fc
Abdurahman <abdi_mo4@outlook.com>
2024-10-07 17:15:01 +0100

    put table above pie chart

--- lib/pages/budget/budget.dart
+++ lib/pages/budget/budget.dart
@@ -152,10 +152,92 @@ class _BudgetPageState extends State<BudgetPage> {
           centerTitle: true,
           backgroundColor: Colors.lightBlue.shade900,
           title: Center(
-              child: Text("Budget", style: TextStyle(color: Colors.white))),
+              child: Text("Monthly Expenses",
+                  style: TextStyle(color: Colors.white))),
         ),
         body: SingleChildScrollView(
           child: Column(children: [
+            FutureBuilder(
+                future: UserServices().getAllUserExpenses(userId),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    var results = snapshot.data as List<Expenses>;
+                    if (results.isNotEmpty) {
+                      return Center(
+                        child: Column(
+                          children: [
+                            SingleChildScrollView(
+                              child: Padding(
+                                padding: const EdgeInsets.all(10),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color: Colors.lightBlue.shade500),
+                                  ),
+                                  child: DataTable(
+                                    dataRowMaxHeight:
+                                        double.infinity, // Code to be changed.
+                                    dataRowMinHeight:
+                                        60, // Set the min required height.
+                                    dividerThickness: 1,
+                                    headingRowColor:
+                                        WidgetStateColor.resolveWith(
+                                      (states) => Colors.lightBlue.shade800,
+                                    ),
+                                    columnSpacing: 50,
+                                    columns: [
+                                      DataColumn(
+                                          label: Text(
+                                        'Name',
+                                        style: TextStyle(color: Colors.white),
+                                      )),
+                                      DataColumn(
+                                          label: Text(
+                                        'Cost',
+                                        style: TextStyle(color: Colors.white),
+                                      )),
+                                      DataColumn(
+                                          label: Text(
+                                        'Edit',
+                                        style: TextStyle(color: Colors.white),
+                                      )),
+                                      DataColumn(
+                                          label: Text(
+                                        'Delete',
+                                        style: TextStyle(color: Colors.white),
+                                      )),
+                                    ],
+                                    rows: List.generate(
+                                      results.length,
+                                      (index) => getDataRow(
+                                        index,
+                                        results[index],
+                                      ),
+                                    ),
+                                    showBottomBorder: true,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    } else {
+                      return Center(
+                        child: Text("No expenses yet!"),
+                      );
+                    }
+                  } else {
+                    return Center(
+                      child: SizedBox(
+                        // ignore: sort_child_properties_last
+                        child: CircularProgressIndicator(),
+                        width: 30,
+                        height: 30,
+                      ),
+                    );
+                  }
+                }),
             Padding(
               padding: const EdgeInsets.all(20),
               child: Column(
@@ -228,83 +310,6 @@ class _BudgetPageState extends State<BudgetPage> {
                 ],
               ),
             ),
-            FutureBuilder(
-                future: UserServices().getAllUserExpenses(userId),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    var results = snapshot.data as List<Expenses>;
-                    if (results.isNotEmpty) {
-                      return Center(
-                        child: Column(
-                          children: [
-                            SingleChildScrollView(
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  border: Border.all(
-                                      color: Colors.lightBlue.shade500),
-                                ),
-                                child: DataTable(
-                                  dataRowMaxHeight:
-                                      double.infinity, // Code to be changed.
-                                  dataRowMinHeight:
-                                      80, // Set the min required height.
-                                  dividerThickness: 1,
-                                  headingRowColor: WidgetStateColor.resolveWith(
-                                    (states) => Colors.lightBlue.shade800,
-                                  ),
-                                  columnSpacing: 30,
-                                  columns: [
-                                    DataColumn(
-                                        label: Text(
-                                      'Name',
-                                      style: TextStyle(color: Colors.white),
-                                    )),
-                                    DataColumn(
-                                        label: Text(
-                                      'Cost',
-                                      style: TextStyle(color: Colors.white),
-                                    )),
-                                    DataColumn(
-                                        label: Text(
-                                      'Edit',
-                                      style: TextStyle(color: Colors.white),
-                                    )),
-                                    DataColumn(
-                                        label: Text(
-                                      'Delete',
-                                      style: TextStyle(color: Colors.white),
-                                    )),
-                                  ],
-                                  rows: List.generate(
-                                    results.length,
-                                    (index) => getDataRow(
-                                      index,
-                                      results[index],
-                                    ),
-                                  ),
-                                  showBottomBorder: true,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    } else {
-                      return Center(
-                        child: Text("No expenses yet!"),
-                      );
-                    }
-                  } else {
-                    return Center(
-                      child: SizedBox(
-                        // ignore: sort_child_properties_last
-                        child: CircularProgressIndicator(),
-                        width: 30,
-                        height: 30,
-                      ),
-                    );
-                  }
-                }),
           ]),
         ));
   }
