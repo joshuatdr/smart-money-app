309a6ff0df66d1f81f7493c6b46cdcbb86b7ed96
joshuatdr <joshuaroper513@gmail.com>
2024-10-07 13:38:21 +0100

    fix overflow on transaction history tab

--- lib/pages/history.dart
+++ lib/pages/history.dart
@@ -74,12 +74,25 @@ class _HistoryScreenState extends State<HistoryScreen> {
     DataRow getDataRow(index, data) {
       return DataRow(
         cells: <DataCell>[
-          (DataCell(Expanded(child: Text(data.name,  maxLines: 6,
-  softWrap: true,)))),
-          DataCell(Expanded(child: Text('£${data.cost.toStringAsFixed(2)}', maxLines: 2,
-  softWrap: false,),)),
-          DataCell(Expanded(child: Text(data.createdAt,maxLines: 2,
-  softWrap: false,))),
+          (DataCell(Expanded(
+              child: Text(
+            data.name,
+            maxLines: 6,
+            softWrap: true,
+          )))),
+          DataCell(Expanded(
+            child: Text(
+              '£${data.cost.toStringAsFixed(2)}',
+              maxLines: 2,
+              softWrap: false,
+            ),
+          )),
+          DataCell(Expanded(
+              child: Text(
+            data.createdAt,
+            maxLines: 2,
+            softWrap: false,
+          ))),
           DataCell(
             Icon(Icons.image_outlined, color: Colors.lightBlue[500]),
           ),
@@ -139,11 +152,11 @@ class _HistoryScreenState extends State<HistoryScreen> {
           if (snapshot.hasData) {
             var results = snapshot.data as List<Transactions>;
             if (results.isNotEmpty) {
-              return Center(
-                child: Column(
-                  children: [
-                    SingleChildScrollView(
-                      child: Container(
+              return SingleChild(
+                child: Center(
+                  child: Column(
+                    children: [
+                      Container(
                         decoration: BoxDecoration(
                           border: Border.all(color: Colors.lightBlue.shade500),
                         ),
@@ -153,29 +166,40 @@ class _HistoryScreenState extends State<HistoryScreen> {
                           dataRowMinHeight: 80, // Set the min required height.
                           dividerThickness: 1,
                           headingRowColor: WidgetStateColor.resolveWith(
-                            (states) =>
-                                Colors.lightBlue.shade800,
+                            (states) => Colors.lightBlue.shade800,
                           ),
                           columnSpacing: 30,
                           columns: [
-                          DataColumn(
-                          label: Text('Name',
-                            style: TextStyle(color: Colors.white),)),
-                          DataColumn(
-                          label: Text('Cost',
-                            style: TextStyle(color: Colors.white),)),
-                          DataColumn(
-                          label: Text('Date',
-                            style: TextStyle(color: Colors.white),)),
-                          DataColumn(
-                          label: Text('Image',
-                            style: TextStyle(color: Colors.white),)),
-                          DataColumn(
-                          label: Text('Edit',
-                            style: TextStyle(color: Colors.white),)),
-                          DataColumn(
-                          label: Text('Delete',
-                            style: TextStyle(color: Colors.white),)),
+                            DataColumn(
+                                label: Text(
+                              'Name',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                            DataColumn(
+                                label: Text(
+                              'Cost',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                            DataColumn(
+                                label: Text(
+                              'Date',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                            DataColumn(
+                                label: Text(
+                              'Image',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                            DataColumn(
+                                label: Text(
+                              'Edit',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                            DataColumn(
+                                label: Text(
+                              'Delete',
+                              style: TextStyle(color: Colors.white),
+                            )),
                           ],
                           rows: List.generate(
                             results.length,
@@ -187,8 +211,8 @@ class _HistoryScreenState extends State<HistoryScreen> {
                           showBottomBorder: true,
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               );
             } else {
